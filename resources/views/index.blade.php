52cc83359cc927636d7efeb18f00af57c692da47
farindra <farindra.project@gmail.com>
2020-04-10 19:36:48 +0700

    add google tag code

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,6 +11,15 @@
 
         gtag('config', 'UA-163324296-1');
       </script>
+      <!-- Google Tag Manager -->
+      <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5W9S5BR');
+      </script>
+        <!-- End Google Tag Manager -->
     @endif
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -29,6 +38,10 @@
     
   </head>
   <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5W9S5BR" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    
     <!-- Fixed navbar -->
     <nav class="navbar navbar-default navbar-fixed-top probootstrap-navbar">
       <div class="container">
