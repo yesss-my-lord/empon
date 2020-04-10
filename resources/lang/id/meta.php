7dc68ad45b77febbe8836d03b1b7de6a7135cffb
farindra <farindra.project@gmail.com>
2020-04-10 15:35:34 +0700

    update meta

--- resources/lang/id/meta.php
+++ resources/lang/id/meta.php
@@ -3,6 +3,6 @@
         'title' => "Empon-empon Obat Herbal Pencegah Corona",
         'description' => "Empon-empon merupakan rangkaian rempah-rempah mengandung curcumin dan magnesium seperti jahe, temulawak, kunyit, kayu manis, dan  ditambah pemanis alami gula merah yang diseduh kemudian diminum terbukti dapat meningkatkan ketahanan/imun tubuh secara alami",
         'keywords' => "empon-empon, mpon-mpon, obat corona, obat covid-19, obat jahe seduh, jual obat herbal seduh",
-        'image' => "https://empon.id/storage/data/images/empon-og.png",
+        'image' => "https://empon.id/storage/data/images/empon-og.jpg",
         
     ];
